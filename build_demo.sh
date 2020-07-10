32a92f687ca3d076c6eae6cf01443fdaf6f47c2f
Alberto Gonzalez <alberto.gonzalez@hyve.net>
2020-07-10 16:36:32 +0100

    add gnofract4d as git submodule

--- build_demo.sh
+++ build_demo.sh
@@ -10,10 +10,10 @@ emsdk/emsdk activate latest
 source emsdk/emsdk_env.sh
 
 emcc -O3 -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' \
-    -I . -I model \
+    -I gnofract4d/fract4d/c -I gnofract4d/fract4d/cmodel \
     demo.cpp \
-    model/*.cpp \
-    fract_stdlib.cpp \
+    gnofract4d/fract4d/c/model/*.cpp \
+    gnofract4d/fract4d/c/fract_stdlib.cpp \
     custom_mandelbrot_formula.c
 
 # use node to serve the files: which is included into the emsdk toolchain
